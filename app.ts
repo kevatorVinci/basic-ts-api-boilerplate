d1190197a1ab0e7aa9ee92c6e8e3f20db27a5796
Akator <kevin.akator@student.vinci.be>
2024-09-29 18:15:53 +0200

    Your descriptive commit message here

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import usersRouter from "./routes/users";
 import pizzaRouter from "./routes/pizzas";
+import drinkRouter from "./routes/drinks";
 
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/users", usersRouter);
 app.use("/pizzas", pizzaRouter);
+app.use("/drink",drinkRouter);
 
 
   
