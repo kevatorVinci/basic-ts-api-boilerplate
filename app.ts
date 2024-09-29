b796602c8e106766aad7e5d16076cc9572ee5575
Akator <kevin.akator@student.vinci.be>
2024-09-29 19:08:22 +0200

    update drinks

--- app.ts
+++ app.ts
@@ -10,9 +10,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+
 app.use("/users", usersRouter);
 app.use("/pizzas", pizzaRouter);
-app.use("/drink",drinkRouter);
+app.use("/drinks",drinkRouter);
 
 
   
